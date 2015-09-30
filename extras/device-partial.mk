4cb232fed7e4ec351f4f1a7fb393f55184c7b2f1
rvkasper <rvkasper@gmail.com>
2015-09-30 15:28:45 -0600

    initial commit for extras, flounder, grouper and tilapia

---
+++ extras/device-partial.mk
@@ -0,0 +1,9 @@
+# Extra Packages and Files
+PRODUCT_COPY_FILES += \
+    vendor/extras/supersu/SuperSU.zip:system/supersu/SuperSU.zip \
+    vendor/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
+    vendor/extras/bootanimation/bootanimation.zip:system/media/bootanimation.zip \
+
+ PRODUCT_PACKAGES += \
+    AdAway \
+    MiXplorer
