03b8f7a31273bf2ccd6499fd8d82ec34aa03a53e
bshiznit <rvkasper@gmail.com>
2016-09-02 15:04:34 -0600

    Add sysinit from CM:

--- extras/device-partial.mk
+++ extras/device-partial.mk
@@ -3,6 +3,7 @@ PRODUCT_COPY_FILES += \
     vendor/extras/supersu/SuperSU.zip:system/supersu/SuperSU.zip \
     vendor/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
     vendor/extras/bootanimation/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/extras/bin/sysinit:system/bin/sysinit \
 
  PRODUCT_PACKAGES += \
     AdAway \
