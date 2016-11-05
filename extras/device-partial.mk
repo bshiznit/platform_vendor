d31cd49b7b78fd84687eb7c8f6cf10e6f939adc5
bshiznit <rvkasper@gmail.com>
2016-11-05 13:05:29 -0600

    lcase supersu, replace sysinit with sysinit.sh

--- extras/device-partial.mk
+++ extras/device-partial.mk
@@ -1,10 +1,11 @@
 # Extra Packages and Files
 PRODUCT_COPY_FILES += \
-    vendor/extras/supersu/SuperSU.zip:system/supersu/SuperSU.zip \
+    vendor/extras/supersu/supersu.zip:supersu/supersu.zip \
     vendor/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
     vendor/extras/bootanimation/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/extras/bin/sysinit:system/bin/sysinit \
+    vendor/extras/supersu/install-recovery.sh:system/etc/install-recovery.sh \
+    vendor/extras/sbin/sysinit.sh:root/sbin/sysinit.sh \
 
  PRODUCT_PACKAGES += \
     AdAway \
-    MiXplorer
+    AmazeFileManager
