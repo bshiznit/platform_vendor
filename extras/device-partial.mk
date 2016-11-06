faa8839c075425831b59b9fd49711832e09a978e
bshiznit <rvkasper@gmail.com>
2016-11-05 18:54:28 -0600

    sysinit.sh and init.d_support scripts copied to RAMDISK

--- extras/device-partial.mk
+++ extras/device-partial.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
     vendor/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
     vendor/extras/bootanimation/bootanimation.zip:system/media/bootanimation.zip \
     vendor/extras/supersu/install-recovery.sh:system/etc/install-recovery.sh \
-    vendor/extras/sbin/sysinit.sh:root/sbin/sysinit.sh \
+    vendor/extras/rootdir/sbin/sysinit.sh:root/sbin/sysinit.sh \
     vendor/extras/rootdir/init.d_support.sh:root/init.d_support.sh \
 
  PRODUCT_PACKAGES += \
